acc2f5f8908593e08176dd9336c8d2c9f9d27db6
felipe augusto <felipe.arnaud@aluno.uece.br>
2025-01-23 18:46:41 -0300

    :lock: chore: adicionando arquivo de configuração padrão do spring

---
+++ src/main/java/com/github/felipeaaa1/libraryapi/config/SecurityConiguration.java
@@ -0,0 +1,28 @@
+package com.github.felipeaaa1.libraryapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConiguration {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> {
+                    authorize.anyRequest().authenticated();
+                })
+                .build();
+    }
+}
